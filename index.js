45cb267b725bfb9973dd1c84834627796e1f4ed3
Srivathsa PV <srivathsa@realimage.com>
2016-12-29 13:37:30 +0530

    Reformat package.json and add .gitignore

--- index.js
+++ index.js
@@ -3,6 +3,34 @@
 const _ = require('lodash')
 const jsonPath = require('json-path');
 
-module.exports = function(json, path) {
+
+const ARRAY_IDENTIFIER = '[*]'
+const PATH_SEPARATOR = '/'
+
+module.exports = function(json, paths) {
+	if(!_.isArray(paths)) {
+		paths = [paths]
+	}
+
+	let unsetObject = _.cloneDeep(json)
+	paths.forEach(path => {
+		unsetObject = _unsetNestedAttribute(unsetObject, path)
+	})
+	return unsetObject
+}
+
+function _unsetNestedAttribute(json, path) {
+	let parts = path.split(PATH_SEPARATOR)
+	let parentPath = _.take(parts, parts.length - 1).join(PATH_SEPARATOR)
+	let terminalAttribute = parts[parts.length - 1].replace('[*]', '')
+
+	let parentObject = jsonPath.resolve(json, parentPath)
+	if(_.isArray(parentObject)) {
+		parentObject.forEach(e => {
+			delete e[terminalAttribute]
+		})
+	} else {
+		delete parentObject[terminalAttribute]
+	}
 	return json
 }
